10e657cedc398d60be2d879feea5e15aa15870b2
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-15 02:15:21 -0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# React App - Tic-Tac-Toe
-
-### This project was realized as a avaliation, his original path is in the repository 'trybe-projects', with all commit history.
---
+++ precache-manifest.63d837885ce7de97483b9e25b96236dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b2fc7112f78aef7882d18356642ef3",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "3c94a3d553a79b542782",
+    "url": "/frontend-online-store/static/css/main.9335f959.chunk.css"
+  },
+  {
+    "revision": "556a5be6e52035ae922b",
+    "url": "/frontend-online-store/static/js/2.f0a4bf06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-online-store/static/js/2.f0a4bf06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c94a3d553a79b542782",
+    "url": "/frontend-online-store/static/js/main.97efb574.chunk.js"
+  },
+  {
+    "revision": "a2d408d9505863c5fc2c",
+    "url": "/frontend-online-store/static/js/runtime-main.2928fc3e.js"
+  }
+]);
